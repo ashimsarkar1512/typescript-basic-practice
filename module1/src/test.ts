0d61ba6cc28f589e3799cbe821db4fdbd9cf03da
ashimsarkar1512 <ashimsarkar5558@gmail.com>
2024-11-05 21:50:44 +0600

    practice basic

---
+++ module1/src/test.ts
@@ -0,0 +1,35 @@
+// number
+
+let roll:number=23;
+
+// string
+
+let firstname : string ="ashim";
+
+// boolean
+
+let isAdmin:boolean=true;
+
+// null
+
+let x:null=null;
+
+// undefined
+ let y : undefined=undefined;
+
+
+
+//  Array
+
+
+let friends:string[]=["ashim","anik","akash"]
+
+friends.push("ashish")
+
+
+let rollNumber:number[]=[1,2,3]
+
+rollNumber.push(6)
+
+
+
