1a87eaedfd7f3d0484ebcdac4366f878ea4d1b06
ashimsarkar1512 <ashimsarkar5558@gmail.com>
2024-11-06 02:06:59 +0600

    basics practice typescript

---
+++ module1/src/1.6.js
@@ -0,0 +1,8 @@
+// learning function
+// normal funtion 
+// arrow function
+function add(num1, num2) {
+    return num1 + num2;
+}
+add(2, 3);
+var addArrow = function (num1, num2) { return num1 + num2; };
